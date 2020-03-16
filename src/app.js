1c9d55f03dedd2f450fa8da8149fe2b4dc15b9da
esparzou <esparzou@gmail.com>
2020-03-16 19:11:43 +0100

    :wrench: Add Config.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import config from "./config";
 const app = express();
+
+// Config
+config(app);
 
 app.listen(6060, () =>
   console.log("El servidor ha sido inicializado: http://localhost:6060")
