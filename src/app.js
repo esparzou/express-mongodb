2a87055e794ffdac9d081c5d320d9b47a98b045d
esparzou <esparzou@gmail.com>
2020-03-17 10:35:19 +0100

    :sparkles: Add Router.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import config from "./config";
+import router from "./router";
 const app = express();
 
 // Config
 config(app);
+
+// Router
+router(app);
 
 app.listen(process.env.PORT, () =>
   console.log(`El servidor ha sido inicializado: http://${process.env.HOST}:${process.env.PORT}`)
